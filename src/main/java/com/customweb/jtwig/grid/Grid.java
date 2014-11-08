5c5691fda0dd608b584a375454f9e9a0a3932640
Thomas Hunziker <thomas.hunziker@customweb.com>
2014-11-08 15:11:37 +0100

    First experimence with jTwig.

---
+++ src/main/java/com/customweb/jtwig/grid/Grid.java
@@ -0,0 +1,45 @@
+package com.customweb.jtwig.grid;
+
+import com.lyncode.jtwig.addons.AddonModel;
+import com.lyncode.jtwig.compile.CompileContext;
+import com.lyncode.jtwig.content.api.Renderable;
+import com.lyncode.jtwig.exception.CompileException;
+import com.lyncode.jtwig.exception.RenderException;
+import com.lyncode.jtwig.render.RenderContext;
+
+public class Grid extends AddonModel<Grid> {
+	
+	private final String gridVariableName;
+	
+	public Grid(String gridVariableName) {
+		this.gridVariableName = gridVariableName;
+	}
+
+	@Override
+	public Renderable compile(CompileContext context) throws CompileException {
+		return new Compiled(super.compile(context));
+	}
+	
+	public String getGridVariableName() {
+		return this.gridVariableName;
+	}
+
+	private class Compiled implements Renderable {
+		private final Renderable content;
+
+		private Compiled(Renderable content) {
+			this.content = content;
+		}
+
+		@Override
+		public void render(RenderContext context) throws RenderException {
+			
+			// Prevent access on grid variables outside
+			context = context.isolatedModel();
+			context.with("other", "sample");
+			
+			this.content.render(context);
+		}
+	}
+
+}
